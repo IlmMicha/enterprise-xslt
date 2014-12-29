e0bc56b55d5d160ddba9fcd4fdeefba65b25e8c1
isachse <danielsachse@lucie-macbook-pro.x-case.local>
2014-12-29 04:06:33 +0100

    add: added validation (unix/linux)
    * added validation to .sh
    * delete outputfile before transformation

--- SAP_Function_Categories/Arithmetic/add/80_add.sh
+++ SAP_Function_Categories/Arithmetic/add/80_add.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-java -cp "../../../lib/xalan.jar" org.apache.xalan.xslt.Process -XSLTC -XSL 00-add.xslt -IN 10_add_in.xml -OUT 20_add_out.xml > 90_add.log
+rm 20_add_out.xml 2> /dev/null
+java -cp "../../../lib/xalan.jar" org.apache.xalan.xslt.Process -XSLTC -XSL 00-add.xslt -IN 10_add_in.xml -OUT 20_add_out.xml 2> 90_add.log
+xmllint --schema ../../../xsd/output_sum.xsd 20_add_out.xml 2>> 90_add.log
